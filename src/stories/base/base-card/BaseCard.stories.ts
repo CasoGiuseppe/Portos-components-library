c03180e92e9138f06e0ec5b3239bb35a3994185e
Carlos Zorrilla <zorrillacarlos4@gmail.com>
2024-03-06 13:06:36 -0400

    create BaseCard component, stories and test, and add BaseTable test

---
+++ src/stories/base/base-card/BaseCard.stories.ts
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import BaseCard from '@ui/base/base-card/BaseCard.vue'
+import IconArrowCircleDown from '@ui/icons/IconArrowCircleDown.vue'
+
+const meta: Meta = {
+  title: 'Base/BaseCard',
+  tags: ['autodocs'],
+  component: BaseCard,
+  argTypes: {
+    title: { control: 'text' },
+    body: { control: 'text' }
+  }
+}
+
+export default meta
+
+type Story = StoryObj<typeof BaseCard>
+
+const Templates: Story = {
+  render: (args) => ({
+    components: { BaseCard, IconArrowCircleDown },
+    setup() {
+      return { args }
+    },
+    template: `
+  <div>
+    <BaseCard>
+      <template #title>
+      <p>{{ args.title }}</p>
+        <IconArrowCircleDown />
+      </template>
+      <template #body>
+        <p>{{ args.body }}</p>
+      </template>
+    </BaseCard>
+  </div>
+`
+  })
+}
+
+export const Default: Story = {
+  ...Templates,
+  args: { title: 'Título de ejemplo', body: 'Texto de ejemplo' }
+}
