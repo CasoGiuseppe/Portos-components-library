982eee9a693a52ef6db2340c36b48a63a0fc9b07
Giuseppe CASO <giuseppecaso@macbook-air-de-servicedesk.home>
2024-03-12 21:13:04 +0100

    refactor: remove unnecessary files

--- src/components/base/base-button/BaseButton.vue
+++ src/components/base/base-button/BaseButton.vue
@@ -1,33 +1,58 @@
 <template>
-    
-  <button :class="computedClasses" @handleClick="handleClick" :disabled="props.config.disabled">
-    <slot></slot>
+  <button
+    :id="id"
+    :class="[
+      'base-button',
+      `base-button--is-${type}`,
+      `base-button--is-${size}`,
+      `${variant ? `base-button--is-${type}-${variant}` : ''}`
+    ]"
+    :disabled="disabled"
+    :title="label"
+    :aria-disabled="disabled"
+    :aria-label="label"
+    @click="handleClick"
+  >
+    <slot />
   </button>
 </template>
 
 <script setup lang="ts">
-import { defineEmits } from 'vue'
+import { defineEmits, type PropType } from 'vue'
+import { type UniqueId, Sizes, Types, Variants } from './types';
+import useValidations from '@/components/validation/useValidation';
 
-const emits = defineEmits(['click'])
-const props = defineProps({
-  config: {
-    type: Object,
-    required: true,
+
+defineProps({
+  id: {
+    type: String as PropType<UniqueId>,
+    default: 'buttonId'
+  },
+  type: {
+    type: String as PropType<Types>,
+    default: Types.PRIMARY,
+    validator: (prop: Types) => useValidations().validateValueCollectionExists({ collection: Types, value: prop})
+  },
+  variant: {
+    type: String as PropType<Variants>,
+    validator: (prop: Variants) => useValidations().validateValueCollectionExists({ collection: Variants, value: prop})
+  },
+  size: {
+    type: String as PropType<Sizes>,
+    default:Sizes.L,
+    validator: (prop: Sizes) => useValidations().validateValueCollectionExists({ collection: Sizes, value: prop})
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+  label: {
+    type: String
   }
-})
-
-const computedClasses = {
-  'primary': !props.config.type,
-  'medium': !props.config.size,
-  'disabled': !!props.config.disabled,
-  [props.config.type || '']: !!props.config.type,
-  [props.config.size || '']: !!props.config.size,
-}
-
-const handleClick = () => {
-  emits('click')
-}
+});
+const emits = defineEmits(['click'])
+const handleClick = () => emits('click')
 
 </script>
 
-<style scoped src="./BaseButton.scss"></style>
+<style src="./BaseButtonV2.scss"></style>
