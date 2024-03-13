034ad64289242f8c0338b965dff9fe93d0310aab
@enua <enrique.programador@gmail.com>
2024-03-13 17:24:43 +0100

    (base-textarea): #12918 refactored, mixins, active, disabled, focus, counter, show help

--- src/components/base/base-textarea/BaseTextarea.vue
+++ src/components/base/base-textarea/BaseTextarea.vue
@@ -1,73 +1,121 @@
 <template>
   <div class="base-textarea">
     <!--Header-->
-    <div class="base-textarea_header">
-      <span class="label">
+    <div class="base-textarea--header">
+      <span class="base-textarea--header-label">
         {{ label }}
       </span>
-      <span class="question-mark">
-        <base-icon :name="'IconFeedbackAnswer'" :type="IconType.FEEDBACK"></base-icon>
-      </span>
+      <div class="base-textarea--header-question_mark" @click="showHelp = !showHelp">
+        <base-icon :name="'IconFeedbackAnswer'" :type="Types.FEEDBACK" :size="Sizes.S"></base-icon>
+      </div>
     </div>
     <!--wrapper-->
-    <div class="base-textarea_wrapper">
+    <div class="base-textarea--wrapper">
       <textarea
-        class="textarea"
+        :id="id"
         :value="text"
-        :class="classes && `${hasError ? 'error' : ''}`"
+        :class="['base-textarea--wrapper-textarea', `${hasError ? 'error' : ''}`]"
         :disabled="disabled"
         :placeholder="placeholder"
         @input="updateCharacterCount"
       ></textarea>
-      <div class="clear-text" v-if="!disabled" @click="clearTextarea">
-        <base-icon :name="'IconEditCleanCircle'" :type="IconType.EDIT"> </base-icon>
+      <div class="base-textarea--wrapper-clear_text" v-if="!disabled" @click="clearTextarea">
+        <base-icon :name="'IconEditCleanCircle'" :type="Types.EDIT" :size="Sizes.S"> </base-icon>
       </div>
     </div>
     <!--footer -->
-    <div class="base-textarea_footer">
-      <div class="help-text">
-        <div class="icon-error">
+    <div class="base-textarea--footer">
+      <div class="base-textarea--footer-help_area">
+        <div class="base-textarea--footer-help_area-icon_error">
           <base-icon
-            v-if="hasError"
+            v-if="hasError && showHelp"
             :name="'IconFeedbackError'"
-            :type="IconType.FEEDBACK"
+            :type="Types.FEEDBACK"
+            :size="Sizes.XS"
           ></base-icon>
         </div>
-        {{ helpText }}
+        <div
+          class="base-textarea--footer-help_area-help_text"
+          v-if="showHelp"
+          :class="hasError ? 'error' : ''"
+        >
+          {{ helpText }}
+        </div>
       </div>
-      <div class="counter">{{ characterCount }} / {{ max }}</div>
+      <div class="base-textarea--footer-counter">{{ characterCount }} / {{ max }}</div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, watchEffect } from 'vue'
-import BaseIcon from './../base-icon/BaseIcon.vue'
-import { IconType } from './../base-icon/type'
+import { defineEmits, ref, watchEffect, type PropType } from 'vue'
+import BaseIcon from '@ui/base/base-icon/BaseIcon.vue'
+import { Types, Sizes } from '@ui/base/base-icon/types'
 
 interface TextareaProps {
+  id: string
   label?: string
-  className?: string
   placeholder?: string
   helpText?: string
   disabled?: boolean
-  text?: string
   maxLength?: any
 }
 
-const props = withDefaults(defineProps<TextareaProps>(), {
-  maxLength: 260
+const { id, label, placeholder, helpText, disabled, maxLength }: TextareaProps = defineProps({
+  /**
+   * Set the unique id of the ui button
+   */
+  id: {
+    type: String as PropType<string>,
+    default: 'textareaId'
+  },
+  /**
+   * Set label for the ui textarea
+   */
+  label: {
+    type: String,
+    default: 'Label'
+  },
+  /**
+   * Set placeholder for the ui textarea
+   */
+  placeholder: {
+    type: String,
+    default: 'Write something here!...'
+  },
+  /**
+   * Set the help text when user click on help icon
+   */
+  helpText: {
+    type: String,
+    default: 'Help text'
+  },
+  /**
+   * Set max number characters on ui textarea to show error
+   */
+  maxLength: {
+    type: Number,
+    default: 260
+  },
+  /**
+   * Sets if the ui textarea is disabled
+   */
+  disabled: {
+    type: Boolean,
+    default: false
+  }
 })
-const text = ref(props.text)
-const max = ref(props.maxLength)
+
+const text = ref('')
+const max = ref(maxLength)
 const characterCount = ref(0)
+const emitsError = defineEmits(['textareaError'])
 let hasError = ref(false)
-const classes = ref(`${props.className} ${props.disabled ? 'disabled' : ''}`)
+let showHelp = ref(false)
 
 watchEffect(() => {
-  if (props.text && props.text.length > 0) {
-    text.value = props.text
-    characterCount.value = props.text.length
+  if (hasError.value) {
+    emitsError('textareaError', { error: 'TEXT_SIZE_EXCEEDED' })
   }
 })
 
@@ -75,13 +123,17 @@ const updateCharacterCount = (event: Event) => {
   const target = event.target as HTMLTextAreaElement
   text.value = target.value
   characterCount.value = target.value.length
-
-  hasError.value = target.value.length > max.value
+  hasError.value = characterCount.value > max.value
+  if (hasError.value) {
+    showHelp.value = true
+  }
 }
 
 const clearTextarea = () => {
   text.value = ''
   characterCount.value = 0
+  hasError.value = characterCount.value > max.value
+  if (showHelp.value) showHelp.value = false
 }
 </script>
 
--- src/components/base/base-textarea/BaseTextareaViewer.vue
+++ src/components/base/base-textarea/BaseTextareaViewer.vue
@@ -1,24 +1,20 @@
 <template>
   <BaseTextarea
+    :id="'textarea-1'"
     :label="'label one'"
-    :className="'test'"
     :placeholder="'Type your text...'"
-    :helpText="'Do something!'"
+    :helpText="helpText"
     :disabled="false"
     :text="'I was typing all day long...'"
     :maxLength="20"
+    @textareaError="emitsError"
   ></BaseTextarea>
+  <br />
+  <BaseTextarea></BaseTextarea>
+  <br />
   <BaseTextarea
-    :label="'label active'"
-    :className="'test active'"
-    :placeholder="'Type your text...'"
-    :helpText="'Do something!'"
-    :disabled="false"
-    :text="'I was typing all day long...'"
-  ></BaseTextarea>
-  <BaseTextarea
+    :id="'textarea-3'"
     :label="'label'"
-    :className="'test'"
     :placeholder="'Type your text...'"
     :helpText="'Do something!'"
     :disabled="true"
@@ -27,7 +23,15 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import BaseTextarea from './BaseTextarea.vue'
+
+let helpText = ref('Type since you can...')
+
+const emitsError = (error: any) => {
+  console.log('error')
+  helpText.value = error.error ? 'Max characters are 20' : 'Other message'
+}
 </script>
 
 <style scoped></style>
