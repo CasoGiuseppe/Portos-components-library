727bc746e35547a70f755943b462982e9a3d7c36
@enua <enrique.programador@gmail.com>
2024-03-14 10:14:39 +0100

    (base-textarea): #12918 clean some unnecesary comments & add scss var for outline color

--- src/components/base/base-textarea/__tests__/BaseTextarea.spec.ts
+++ src/components/base/base-textarea/__tests__/BaseTextarea.spec.ts
@@ -16,21 +16,16 @@ describe('BaseTextarea', () => {
 
   describe('Error icon and help text display', () => {
     it('shows error icon and help text when character count exceeds maxLength', async () => {
-      // Montamos el componente con una longitud máxima de caracteres reducida para simular el exceso
       const wrapper = mount(BaseTextarea, {
         props: {
-          maxLength: 5 // Establecer una longitud máxima baja para forzar el error
+          maxLength: 5
         }
       })
 
-      // Encontramos el área de texto y establecemos un valor que exceda la longitud máxima
       const textarea = wrapper.find('textarea')
       await textarea.setValue('This is too long')
-
-      // Esperamos que se actualice el estado del componente
       await wrapper.vm.$nextTick()
 
-      // Verificamos si se muestra el icono de error y el texto de ayuda
       expect(wrapper.find('.base-textarea--footer-help_area-icon_error').exists()).toBe(true)
       expect(wrapper.find('.base-textarea--footer-help_area-help_text.error').text()).toBe(
         'Help text'
@@ -40,27 +35,20 @@ describe('BaseTextarea', () => {
 
   describe('Textarea clearing', () => {
     it('clears textarea when clear button is clicked', async () => {
-      // Montamos el componente
       const wrapper = mount(BaseTextarea)
-
-      // Encontramos el área de texto y establecemos un valor inicial
       const textarea = wrapper.find('textarea')
       await textarea.setValue('Some text')
 
-      // Esperamos que se actualice el estado del componente
       await wrapper.vm.$nextTick()
 
-      // Encontramos el botón de limpieza y hacemos clic en él
       const clearButton = wrapper.find('.base-textarea--wrapper-clear_text')
       await clearButton.trigger('click')
 
-      // Esperamos que se actualice el estado del componente
       await wrapper.vm.$nextTick()
       const vm = wrapper.vm as ComponentPublicInstance<
         TextareaProps & { characterCount: number; text: string }
       >
 
-      // Verificamos si el área de texto se ha limpiado correctamente
       expect(vm.text).toBe('')
       expect(vm.characterCount).toBe(0)
     })
@@ -68,25 +56,20 @@ describe('BaseTextarea', () => {
 
   describe('Event emission', () => {
     it('emits textareaError event when character count exceeds maxLength', async () => {
-      // Montamos el componente con una longitud máxima de caracteres reducida para simular el exceso
       const wrapper = mount(BaseTextarea, {
         props: {
-          maxLength: 5 // Establecer una longitud máxima baja para forzar el error
+          maxLength: 5
         }
       })
 
-      // Encontramos el área de texto y establecemos un valor que exceda la longitud máxima
       const textarea = wrapper.find('textarea')
       await textarea.setValue('This is too long')
 
-      // Esperamos que se actualice el estado del componente
       await wrapper.vm.$nextTick()
 
-      // Verificamos si se emitió correctamente el evento textareaError
       const emittedTextareaError = wrapper.emitted('textareaError')
       expect(emittedTextareaError).toBeTruthy()
 
-      // Verificamos si se emitió correctamente el evento con datos correctos
       if (emittedTextareaError) {
         expect(emittedTextareaError[0][0]).toEqual({ error: 'TEXT_SIZE_EXCEEDED' })
       }
