f58c53145f5920ca5844b23ec4efb5a39c317379
Carlos Zorrilla <zorrillacarlos4@gmail.com>
2024-03-15 06:15:57 -0400

    update npm

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,12 @@ import BaseIcon from './components/base/base-icon/BaseIcon.vue';
 import BaseSearch from './components/base/base-search/BaseSearch.vue';
 
 export * from './components/icons';
-
-// Importa las utilidades desde la carpeta src/shared
 export * from './shared/helpers';
-// export * from './components/base/base-icon/type'
-export * from './components/base/base-table/interfaces'
 
 // Exporta los componentes individualmente
 export { BaseCard, BaseTable, BaseIcon, BaseSearch };
 
-// Proporciona una función de instalación para la instalación global
+// Proporcionar una función de instalación para la instalación global
 export function install(app: App): void {
   app.component('BaseCard', BaseCard);
   app.component('BaseTable', BaseTable);
