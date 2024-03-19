f5ee9bfe06c45be43815e5678ce89ed0f47fc566
Carlos Zorrilla <zorrillacarlos4@gmail.com>
2024-03-19 05:35:55 -0400

    remove component link in App

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,5 @@
 <template>
-  <section>
-    <ViewBaseLink />
-  </section>
+  <section></section>
 </template>
 <script setup lang="ts">
-import ViewBaseLink from './components/base/base-link/ViewBaseLink.vue'
 </script>
-<style>
-body, html{
-  background-color:#0120AF ;
-  width: 100%;
-  height: 100%;
-}
-</style>
