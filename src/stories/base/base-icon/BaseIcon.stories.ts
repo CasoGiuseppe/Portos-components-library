446279d32758cea6602ab6a5fe01ac8090c9c4df
Giuseppe CASO <giuseppecaso@macbook-air-de-servicedesk.home>
2024-03-19 20:42:13 +0100

    feature: create BaseIcon base story

---
+++ src/stories/base/base-icon/BaseIcon.stories.ts
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import BaseIcon from "@/components/base/base-icon/BaseIcon.vue";
+import { Types, Sizes } from "@ui/base/base-icon/types";
+
+
+const meta = {
+    title: 'Base/Base Icon',
+    component: BaseIcon,
+    tags: ['autodocs'],
+    argTypes: {
+        name: { control: 'text' },
+        type: { control: 'select', options: Object.values(Types) },
+        size: { control: 'select', options: Object.values(Sizes) },
+    },
+    args: {
+        name: 'IconArrowCircleUp',
+        type: Types.ARROW,
+        size: Sizes.S
+    }
+} satisfies Meta<typeof BaseIcon>;
+
+export default meta;
+
+type Story = StoryObj<typeof BaseIcon>;
+
+const Templates: Story = {
+    render: (args) => ({
+        components: { BaseIcon },
+        setup() { return { args } },
+        template: `<BaseIcon v-bind="args" />`,
+    }),
+}
+
+export const Default: Story = {
+    ...Templates,
+    args: {},
+}
