7b614de64fc098cdc3174bef7fb61d2b42afffc9
Giuseppe CASO <giuseppecaso@macbook-air-de-servicedesk.home>
2024-03-19 21:37:29 +0100

    feature: create useAsyncComponent composable

---
+++ src/shared/composables/useAsyncComponent.ts
@@ -0,0 +1,18 @@
+import { defineAsyncComponent, type Component } from "vue";
+import type { IAsyncComponent } from "./interfaces/useAsyncComponent";
+import UserDefaultError from "@ui/defaults/exceptions/error/Error.vue";
+// import UserDefaultLoader from "@ui/components/defaults/default-loader/DefaultLoader.vue";
+
+export default function useAsyncComponent(): IAsyncComponent {
+  const create = async ({ component, error = UserDefaultError } :{ component: string, error?: Component, loader?: Component}): Promise<Component> => {
+    return await defineAsyncComponent({
+      errorComponent: error,
+    //   loadingComponent: loader,
+      loader: () => import (/* @vite-ignore */ `../../ui/${ component }.vue`)
+    })
+  }
+
+  
+
+  return { create }
+}
