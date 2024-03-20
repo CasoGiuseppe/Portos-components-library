81113537aba7a3fa3ffd910bb355f0921c9fdf80
Carlos Zorrilla <zorrillacarlos4@gmail.com>
2024-03-20 10:28:00 -0400

    add stories for BaseLink

---
+++ src/stories/base/base-link/BaseLink.stories.ts
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import BaseLink from '@/components/base/base-link/BaseLink.vue'
+import { Element, Sizes } from '@/components/base/base-link/types'
+
+const meta: Meta = {
+  title: 'Base/v1.0/Base Link',
+  component: BaseLink,
+  tags: ['autodocs'],
+  args: {
+    label: 'Base Link',
+    size: Sizes.M,
+    variant: false,
+    disabled: false,
+    elementType: Element.ANCHOR,
+    href: 'https://amaris.com/',
+  },
+  argTypes: {
+    size: { control: 'select', options: Object.values(Sizes) },
+    elementType: { control: 'select', options: Object.values(Element) },
+    href: { control: 'text' },
+  }
+}
+
+export default meta
+
+type Story = StoryObj
+
+const Templates: Story = {
+  render: (args) => ({
+    components: { BaseLink },
+    setup() {
+      return { args }
+    },
+    template: `
+        <section>
+          <BaseLink v-bind="args">
+            <template #default>{{ args.label }}</template>
+          </BaseLink>
+        </section>
+    `
+  })
+}
+
+export const Default: Story = {
+  ...Templates,
+  args: {}
+}
