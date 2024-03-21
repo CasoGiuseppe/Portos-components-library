21e411c57ec00d1e0503def252293b037a0089aa
Carlos Zorrilla <zorrillacarlos4@gmail.com>
2024-03-21 10:53:44 -0400

    update library component npm: add new components

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,44 @@
-// Importa los componentes y las historias desde la carpeta src/stories
+// Importar los componentes
 import type { App } from 'vue'
+// base
 import BaseButton from './components/base/base-button/BaseButton.vue'
 import BaseCard from './components/base/base-card/BaseCard.vue'
 import BaseIcon from './components/base/base-icon/BaseIcon.vue'
 import BaseInput from './components/base/base-input/BaseInput.vue'
 import BaseSearch from './components/base/base-search/BaseSearch.vue'
 import BaseTable from './components/base/base-table/BaseTable.vue'
+// nav
+import NavigationItem from './components/navigation/navigation-item/NavigationItem.vue'
+// defaults
+import DefaultError from './components/defaults/exceptions/default-error/DefaultError.vue'
+import DefaultLoader from './components/defaults/loaders/default-loader/DefaultLoader.vue'
+
+// Exporta los componentes
+export {
+  BaseButton,
+  BaseCard,
+  BaseIcon,
+  BaseInput,
+  BaseSearch,
+  BaseTable,
+  NavigationItem,
+  DefaultLoader,
+  DefaultError
+}
 
 export * from './components/icons'
 export * from './shared/helpers'
 export * from './components/validation/useValidation'
 
-// Exporta los componentes individualmente
-export { BaseCard, BaseTable, BaseIcon, BaseSearch, BaseInput, BaseButton }
-
 // Proporcionar una función de instalación para la instalación global
 export function install(app: App): void {
   app.component('BaseButton', BaseButton)
-  app.component('BaseInput', BaseInput)
   app.component('BaseCard', BaseCard)
-  app.component('BaseTable', BaseTable)
   app.component('BaseIcon', BaseIcon)
+  app.component('BaseInput', BaseInput)
   app.component('BaseSearch', BaseSearch)
+  app.component('BaseTable', BaseTable)
+  app.component('NavigationItem', NavigationItem)
+  app.component('DefaultLoader', DefaultLoader)
+  app.component('DefaultError', DefaultError)
 }
