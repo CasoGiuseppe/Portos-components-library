64c140e519a1ecc59ac3a787b3d07b1f12ab2d2b
Giuseppe CASO <giuseppecaso@macbook-air-de-servicedesk.home>
2024-03-25 23:31:06 +0100

    spike: discover observer composable solution

---
+++ src/shared/composables/interfaces/useResizeObserver.ts
@@ -0,0 +1,3 @@
+export interface IResizeObserver {
+  createObserver({ trigger }: { trigger?: HTMLElement | Element }): void,
+  }
---
+++ src/shared/composables/useResizeObserver.ts
@@ -0,0 +1,21 @@
+import type { IResizeObserver } from './interfaces/useResizeObserver';
+
+export default function useResizeObserver({ action } : { action?: (e:any) => any }): IResizeObserver {
+  const createObserver = ({ trigger = document.body }: { trigger?: HTMLElement | Element }) : void => {
+    const observer = _settingObserver()
+    observer.observe(trigger);
+  };
+
+  const _settingObserver = () : ResizeObserver => {
+    const observerEvent = new ResizeObserver(async (mutations: any) => {
+        for(const mutation of mutations) {
+          if(action) action(mutation.contentRect)
+        }
+    });
+
+    observerEvent.disconnect()
+    return observerEvent
+  };
+
+  return { createObserver }
+}
