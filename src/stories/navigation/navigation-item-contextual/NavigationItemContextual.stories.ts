92d1ea76ff63d74d9d02fe117a5213d6e4e35626
@enua <enrique.programador@gmail.com>
2024-03-26 16:27:39 +0100

    (13567): generate storybook view

---
+++ src/stories/navigation/navigation-item-contextual/NavigationItemContextual.stories.ts
@@ -0,0 +1,65 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+import NavigationItemContextual from '@/components/navigation/contextual/navigation-item/NavigationItemContextual.vue'
+import BaseIcon from '@/components/base/base-icon/BaseIcon.vue'
+
+const meta = {
+  title: 'Navigation/contextual',
+  component: NavigationItemContextual,
+  tags: ['autodocs'],
+  argTypes: {
+    id: { control: 'text' },
+    selected: { expanded: true }
+  },
+  args: {
+    id: '0',
+    selected: false,
+    content: 'Content tab'
+  }
+} satisfies Meta<typeof NavigationItemContextual>
+
+export default meta
+
+type Story = StoryObj<typeof NavigationItemContextual>
+
+const Templates: Story = {
+  render: (args) => ({
+    components: { NavigationItemContextual, BaseIcon },
+    setup() {
+      return { args }
+    },
+    template: `
+            <section style="display: grid; gap: 10px; grid-template-columns: repeat(6, 1fr);">
+                <NavigationItemContextual
+                    v-bind="args"
+                    
+                >
+                    <template #iconLeft>
+                        <Suspense>
+                            <BaseIcon
+                                name="IconArrowCircleRight"
+                                type="arrow"
+                                :size="'S'"
+                            />
+                        </Suspense>
+                    </template>
+                    <template #content>{{ args.content }}</template>
+                    <template #iconRight>
+                        <Suspense>
+                            <BaseIcon
+                                name="IconArrowCircleLeft"
+                                type="arrow"
+                                :size="'S'"
+                            />
+                        </Suspense>
+                    </template>
+                </NavigationItemContextual>
+            </section
+        `
+  })
+}
+
+export const Default: Story = {
+  ...Templates,
+  args: {}
+}
