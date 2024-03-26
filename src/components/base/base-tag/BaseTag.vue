c0ae02f7decf1d7d7fe74948f55547804859fa57
Carlos Zorrilla <zorrillacarlos4@gmail.com>
2024-03-26 11:39:13 -0400

    change template in BaseTag

--- src/components/base/base-tag/BaseTag.vue
+++ src/components/base/base-tag/BaseTag.vue
@@ -1,28 +1,26 @@
 <template>
-  <section :class="['base-tag', `base-tag--is-${status || Types.Default}`]">
+  <span :class="['base-tag', `base-tag--is-${status || Types.Default}`]">
     <Suspense>
       <BaseIcon :size="Sizes.S" :type="statusIcon[status]?.type" :name="statusIcon[status]?.name" />
     </Suspense>
-    <span>
-      {{ status === 'damage' ? `${damage} Daños` : status === 'warning' ? 'Bloqueada' : 'Default' }}
-    </span>
-  </section>
+    {{ status === 'damage' ? `${damage} Daños` : status === 'warning' ? 'Bloqueada' : 'Default' }}
+  </span>
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue';
-import { type Name, Types } from './types';
-import BaseIcon from '@ui/base/base-icon/BaseIcon.vue';
-import { Types as TypesICon, Sizes } from '@ui/base/base-icon/types';
+import { defineProps } from 'vue'
+import { type Name, Types } from './types'
+import BaseIcon from '@ui/base/base-icon/BaseIcon.vue'
+import { Types as TypesICon, Sizes } from '@ui/base/base-icon/types'
 
-const { status, damage } = defineProps<Name>();
+const { status, damage } = defineProps<Name>()
 
 // Icons for status reference
 const statusIcon = {
   damage: { type: TypesICon.FEEDBACK, name: 'IconFeedbackCheck' },
   warning: { type: TypesICon.LOCATION, name: 'IconLocationCar' },
   [Types.Default]: { type: TypesICon.ARROW, name: 'IconArrowCircleDown' }
-};
+}
 </script>
 
 <style src="./BaseTag.scss" lang="scss"></style>
