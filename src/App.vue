eef612be2d38238efa1b11dd6a70b67eef0edec8
Giuseppe CASO <giuseppecaso@macbook-air-de-servicedesk.home>
2024-03-26 09:58:55 +0100

    refactor: remove unnecessary code from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,6 @@
 <template>
-  <section
-    ref="observer"
-    style="width: 100%;
-    height: 100vh;
-    background-color: red;"
-  >
-    observable
-  </section>
+  <section></section>
 </template>
 
 <script setup lang="ts">
-import useResizeObserver from "@/shared/composables/useResizeObserver"
-import { onMounted, ref } from "vue";
-const observer = ref(null);
-
-const showResizeData = ({ width, height }: { width: number, height: number}): any => console.table({ width, height })
-const { createObserver } = useResizeObserver({ action: showResizeData });
-
-onMounted(() => {
-  if(!observer.value) return;
-  createObserver({
-    trigger: observer.value
-  })
-})
 </script>
