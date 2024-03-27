df27245e15a0f70b483a9bd31ca182b643c578dc
Giuseppe CASO <giuseppecaso@macbook-air-de-servicedesk.home>
2024-03-27 23:14:35 +0100

    fix: remove unnecessary code

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,6 @@
 <template>
-  <div style="display: flex; justify-content: center;">
-    <section class="grid">
-      <ul class="grid__row">
-          <div class="grid__col-sm-4 grid__col-md-2 grid__col-lg-3 foundation__grid-col foundation__grid-col--secondary">sm: 4, md: 2, lg: 3</div>
-          <div class="grid__col-sm-4 grid__col-md-2 grid__col-lg-8 foundation__grid-col foundation__grid-col--secondary">sm: 4, md: 2, lg: 8</div>
-          <div class="grid__col-sm-4 grid__col-md-8 grid__col-lg-1 foundation__grid-col foundation__grid-col--secondary">sm: 4, md: 8, lg: 1</div>
-      </ul>
-    </section>
-  </div>
+  <sectio></sectio>
 </template>
 
 <script setup lang="ts">
 </script>
-<style lang="scss">
-.grid {
-  @include text-display-s
-}
-</style>
