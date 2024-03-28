02b4c80ae7d30647d0e4badf6808a8d7c99e556c
Giuseppe CASO <giuseppecaso@macbook-air-de-servicedesk.home>
2024-03-28 21:40:07 +0100

    feature: create basetoggle base component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <sectio></sectio>
+  <section>
+    <BaseToggle>Label</BaseToggle>
+  </section>
 </template>
 
 <script setup lang="ts">
+import BaseToggle from './components/base/base-toggle/BaseToggle.vue';
 </script>
---
+++ src/components/base/base-toggle/BaseToggle.vue
@@ -0,0 +1,24 @@
+<template>
+    <label class="base-toggle">
+        <!-- v-show="false" -->
+        <input
+            type="checkbox"
+            :checked="checked"
+        >
+        <span class="base-toggle__switch"></span>
+        <slot />
+    </label>
+</template>
+<script lang="ts" setup>
+import type { PropType } from 'vue';
+
+defineProps({
+    /**
+   * Set checked state
+   */
+  checked: {
+    type: Boolean as PropType<boolean>,
+    default: true
+  },
+})
+</script>
