106059d270585211675394d5c5be758131080696
Giuseppe CASO <giuseppecaso@macbook-air-de-servicedesk.home>
2024-04-03 16:19:05 +0200

    refactor: create a simple slider code

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <section>
+  <section style="width: 30vw; padding: 10px; background: #ccc;">
+    <Slider></Slider>
   </section>
 </template>
 
 <script setup lang="ts">
+import Slider from "@ui/slider/Slider.vue"
 </script>
