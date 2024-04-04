3cee536a5a2eeb868bad6ddec5390b52d91e5aad
SANOJA Alejandro <sanojaalejandro@Amaris-MacBook-Pro-M1-Pro.local>
2024-04-04 16:37:51 +0200

    [FIX]: NavigationItem unit tests

--- src/components/navigation/main/navigation-item/__tests__/utilities.ts
+++ src/components/navigation/main/navigation-item/__tests__/utilities.ts
@@ -1,6 +1,6 @@
 // slots
 export const $providedIcon = '<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" fill="currentColor"></path></svg>';
-export const $providedChildren = '<ul><li>Children Item</li></ul>';
+export const $providedChild = '<ul><li>Child Item</li></ul>';
 
 // props
 export const $providedId = '1';
