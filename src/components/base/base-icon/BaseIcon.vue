586314fb8795da78411c5891f0ef1d2677f58b11
Carlos Zorrilla <zorrillacarlos4@gmail.com>
2024-04-04 05:17:33 -0400

    fix async issues in BaseIcon component

--- src/components/base/base-icon/BaseIcon.vue
+++ src/components/base/base-icon/BaseIcon.vue
@@ -1,57 +1,63 @@
 <template>
-    <component
-        data-testID="ui-icon"
-        :is="IconAsyncDefine"
-        :id="id"
-        :key="id"
-        :class="[
-            'base-icon',
-            `base-icon--is-${size}`,
-        ]"
-    />
+  <component
+    data-testID="ui-icon"
+    :is="asyncComponent"
+    :id="id"
+    :key="id"
+    :class="['base-icon', `base-icon--is-${size}`]"
+  />
 </template>
 <script lang="ts" setup>
-import { Types, Sizes, type UniqueId } from './types';
-import useAsyncComponent from '@/shared/composables/useAsyncComponent';
-import { validateValueCollectionExists } from '@/components/utilities/validation/useValidation';
-import type { PropType } from 'vue';
+import { Types, Sizes, type UniqueId } from './types'
+import { validateValueCollectionExists } from '@/components/utilities/validation/useValidation'
+import { onMounted, type PropType, ref } from 'vue'
 
-const { create } = useAsyncComponent();
-const { name, type } = defineProps({
-    /**
-    * Set the unique id of the ui button
-    */
-    id: {
-        type: String as PropType<UniqueId>,
-        default: 'iconId'
-    },
-
-    /**
-    * Set icon name to get svg file
-    */
-    name: {
-        type: String as PropType<string>,
-        default: 'IconNavigationCloseS'
-    },
-
-    /**
-    * Set the icon type family
-    */
-    type: {
-        type: String as PropType<Types>,
-        default: Types.NAVIGATION,
-        validator: (prop: Types) => validateValueCollectionExists({ collection: Types, value: prop})
-    },
-
-    /**
-    * Set the icon size [L, M, S, XS]
-    */
-    size: {
-        type: String as PropType<Sizes>,
-        default:Sizes.M,
-        validator: (prop: Sizes) => validateValueCollectionExists({ collection: Sizes, value: prop})
-    },
+onMounted(() => {
+  import(`../../icons/${type}/${name}.vue`)
+    .then((module) => {
+      asyncComponent.value = module.default
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 })
-const IconAsyncDefine = await create({ component: `components/icons/${type}/${name}`})
+
+const asyncComponent = ref(null)
+
+const { name, type } = defineProps({
+  /**
+   * Set the unique id of the ui button
+   */
+  id: {
+    type: String as PropType<UniqueId>,
+    default: 'iconId'
+  },
+
+  /**
+   * Set icon name to get svg file
+   */
+  name: {
+    type: String as PropType<string>,
+    default: 'IconNavigationCloseS'
+  },
+
+  /**
+   * Set the icon type family
+   */
+  type: {
+    type: String as PropType<Types>,
+    default: Types.NAVIGATION,
+    validator: (prop: Types) => validateValueCollectionExists({ collection: Types, value: prop })
+  },
+
+  /**
+   * Set the icon size [L, M, S, XS]
+   */
+  size: {
+    type: String as PropType<Sizes>,
+    default: Sizes.M,
+    validator: (prop: Sizes) => validateValueCollectionExists({ collection: Sizes, value: prop })
+  }
+})
 </script>
 <style src="./BaseIcon.scss" lang="scss"></style>
