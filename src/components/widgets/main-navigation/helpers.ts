369826e56118d6bb07e3d04d27842667258c4fdd
Carlos Zorrilla <zorrillacarlos4@gmail.com>
2024-04-09 12:02:44 -0400

    update npm library 1.2.2

--- src/components/widgets/main-navigation/helpers.ts
+++
@@ -1,7 +0,0 @@
-export const loadNavigationItems = async (userRole: string): Promise<any> => {
-    try {
-        return (await import(`./items/${userRole}`))?.default;
-    } catch (error) {
-        console.error('Error loading items file:', error);
-    }
-};
