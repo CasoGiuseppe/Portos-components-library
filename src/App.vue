b69b7b3ff6cd6ff00b6a8d732e24064f8e716186
@enua <enrique.programador@gmail.com>
2024-04-10 13:17:49 +0200

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,38 @@
 <template>
-  <section style="width: 30vw; padding: 10px; background: #ccc;">
-    <Slider></Slider>
+  <section style="padding: 10px; background: #ccc">
+    <SliderIn :items="sliderItems">
+      <template v-for="item in sliderItems" :key="item.id" v-slot:[`item-${item.id}`]>
+        <NavigationItemContextual :id="item.id" :selected="item.selected" @send="handleSend">{{
+          item.title
+        }}</NavigationItemContextual>
+      </template>
+    </SliderIn>
   </section>
 </template>
 
 <script setup lang="ts">
-import Slider from "@ui/slider/Slider.vue"
+import { ref } from 'vue'
+import SliderIn from '@ui/slider/SliderIn.vue'
+import NavigationItemContextual from './components/navigation/contextual/navigation-item/NavigationItemContextual.vue'
+
+let sliderItems: any = ref([
+  { id: 1, title: 'title small', selected: true },
+  { id: 2, title: 'my title is medium', selected: false },
+  { id: 3, title: 'this is a big title for testing', selected: false },
+  { id: 4, title: 'Noatum', selected: false },
+  { id: 5, title: 'PortOs', selected: false },
+  { id: 6, title: 'Home', selected: false },
+  { id: 7, title: 'Email', selected: false },
+  { id: 8, title: 'Please contact us', selected: false }
+])
+
+const handleSend = (id: number) => {
+  for (let i = 0; i < sliderItems.value.length; i++) {
+    if (sliderItems.value[i].id === id) {
+      sliderItems.value[i].selected = true
+    } else {
+      sliderItems.value[i].selected = false
+    }
+  }
+}
 </script>
---
+++ src/components/slider/SliderIn.vue
@@ -0,0 +1,121 @@
+<template>
+  <!-- Poor Draco Malfoy, never was understood enought-->
+  <section class="slider">
+    <button @click="moveTo({ type: 'prev' })" class="slider--is-prev">
+      <Suspense>
+        <BaseIcon id="card" :type="Types.CHEVRON" name="IconChevronLeftM" :size="Sizes.S" />
+      </Suspense>
+    </button>
+
+    <section class="slider__wrapper">
+      <ul class="slider__list" ref="list">
+        <li v-for="item in items" :key="item.id" class="slider__item">
+          <slot :name="`item-${item.id}`"></slot>
+        </li>
+      </ul>
+    </section>
+    <button @click="moveTo({})" class="slider--is-next">
+      <Suspense>
+        <BaseIcon id="card" :type="Types.CHEVRON" name="IconChevronRightM" :size="Sizes.S" />
+      </Suspense>
+    </button>
+  </section>
+</template>
+<script setup lang="ts">
+import useIntersectionObserver from '@/shared/composables/useIntersectionObserver'
+import { onMounted, ref } from 'vue'
+import BaseIcon from '../base/base-icon/BaseIcon.vue'
+import { Types, Sizes } from '../base/base-icon/types'
+
+const list = ref<HTMLElement | null>(null)
+const currentHTMLNode = ref<HTMLElement | null>(null)
+
+const canMovePrev = ref(true)
+const canMoveNext = ref(true)
+
+withDefaults(defineProps<{ items: any[] }>(), {
+  /**
+   * Set slider items list content
+   */
+  items: () => []
+})
+
+const { createObserver } = useIntersectionObserver({
+  action: (e: any) => {
+    const { isIntersecting } = e
+    e.target.dataset.visible = isIntersecting
+  }
+})
+
+const updateMovePossibility = () => {
+  if (!list.value || !currentHTMLNode.value) {
+    canMoveNext.value = false
+    canMovePrev.value = false
+    return
+  }
+
+  canMovePrev.value = !!currentHTMLNode.value.previousElementSibling
+  canMoveNext.value = !!currentHTMLNode.value.nextElementSibling
+}
+
+const getNextTargetPosition = ({ current }: { current: HTMLElement }): number => {
+  currentHTMLNode.value = current
+  return current.offsetLeft
+}
+
+const moveTo = ({ type = 'next' }: { type?: string }): void => {
+  if ((type === 'prev' && !canMovePrev.value) || (type === 'next' && !canMoveNext.value)) return
+  if (!list.value) return
+  if (!currentHTMLNode.value) return
+  setNewPositionVariable({
+    position: getNextTargetPosition({
+      current: getNextPrevSibling({ element: currentHTMLNode.value, type })
+    })
+  })
+
+  updateMovePossibility()
+}
+
+const setNewPositionVariable = ({ position }: { position: number }): void => {
+  if (!list.value) return
+  list.value.style.setProperty('--slider-position', `${position?.toString()}px` || '0')
+}
+
+const getNextPrevSibling = ({
+  element,
+  type = 'next'
+}: {
+  element: HTMLElement
+  type?: string
+}): HTMLElement => {
+  const target = type === 'next' ? element.nextElementSibling : element.previousElementSibling
+  const HTMLTarget = target as HTMLElement
+  currentHTMLNode.value = HTMLTarget
+
+  return HTMLTarget
+}
+
+onMounted(() => {
+  const listCollection = list.value
+  if (!listCollection) return
+
+  const childElements = Array.from(listCollection.childNodes).filter(
+    (node) => node.nodeType === Node.ELEMENT_NODE
+  ) as HTMLElement[]
+
+  if (childElements.length > 0) {
+    currentHTMLNode.value = childElements[0]
+  }
+
+  currentHTMLNode.value = listCollection.firstChild as HTMLElement
+  childElements.forEach((element) => {
+    createObserver({
+      element: element,
+      options: {
+        threshold: 1
+      }
+    })
+  })
+})
+</script>
+<style src="./SliderIn.scss" lang="scss"></style>
