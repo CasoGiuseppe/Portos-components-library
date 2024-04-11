7d2d570c092eda0cb5b054f2d353ab55d0bc46da
Giuseppe CASO <giuseppecaso@macbook-air-de-servicedesk.home>
2024-04-11 15:31:35 +0200

    refactor: solve slot issue on elements list

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <template>
-  <section style="width: 30vw; padding: 10px; background: #ccc;">
-    <Slider></Slider>
+  <section style="width: 50vw; padding: 10px; background: #ccc;">
+    <Slider :body="body">
+      <template #item="{ property: { label, id } }">
+        <div>{{ label }}{{ id }}</div>
+      </template>
+    </Slider>
   </section>
 </template>
 
 <script setup lang="ts">
 import Slider from "@ui/slider/Slider.vue"
+import { reactive } from "vue";
+const body = reactive([{
+  id: 1,
+  label: 'soy un label',
+  className: 'small',
+},{
+  id: 2,
+  label: 'soy un label',
+  className: 'big'
+},{
+  id: 3,
+  label: 'soy un label',
+  className: 'big'
+},{
+  id: 4,
+  label: 'soy un label',
+  className: 'small'
+},{
+  id: 5,
+  label: 'soy un label',
+  className: 'medium'
+}])
 </script>
--- src/components/slider/Slider.vue
+++ src/components/slider/Slider.vue
@@ -1,18 +1,26 @@
 <template>
     <section class="slider">
-        <button @click="moveTo({ type: 'prev'})" class="slider--is-prev">prev</button>
+        <button
+            @click="moveTo({ type: 'prev'})"
+            class="slider--is-prev"
+            :disabled="isDisabled"
+        >prev</button>
             <section class="slider__wrapper">
                 <ul class="slider__list" ref="list">
-                    <li class="slider__item small">1</li>
-                    <li class="slider__item small">2</li>
-                    <li class="slider__item big">3</li>
-                    <li class="slider__item small">4</li>
-                    <li class="slider__item medium">5</li>
-                    <li class="slider__item small">6</li>
-                    <li class="slider__item medium">7</li>
+                    <li
+                        v-for="{id, label, className } in body"
+                        :key="id"
+                        :class="`slider__item ${className}`"
+                    >
+                        <slot :property="{ label, id }" name="item"></slot>
+                    </li>
                 </ul>
             </section>
-        <button @click="moveTo({})" class="slider--is-next">next</button>
+        <button
+            @click="moveTo({})"
+            class="slider--is-next"
+            :disabled="isDisabled"
+        >next</button>
     </section>
 </template>
 <script setup lang="ts">
@@ -21,6 +29,18 @@ import { onMounted, ref } from 'vue';
 
 const list = ref<HTMLElement | null>(null)
 const currentHTMLNode = ref<HTMLElement | null>(null);
+const isDisabled = ref<boolean>(false);
+
+export interface ISLiderItems {
+    body: Record<string, any>[];
+}
+
+withDefaults(defineProps<ISLiderItems>(), {
+    /**
+     * Set table body content
+     */
+    body: () => [],
+});
 
 const { createObserver } = useIntersectionObserver({
     action: (e: any) => {
@@ -37,6 +57,7 @@ const getNextTargetPosition = ({ current } : { current: HTMLElement }): number =
 const moveTo = ({ type = 'next' }: { type?: string }): void => {
     if(!list.value) return;
     if(!currentHTMLNode.value) return;
+    isDisabled.value = true
     setNewPositionVariable({
         position: getNextTargetPosition({
             current: getNextPrevSibling({element: currentHTMLNode.value, type })
@@ -50,6 +71,8 @@ const setNewPositionVariable = ({ position }: { position: number }): void => {
         '--slider-position',
         `${position?.toString()}px` || '0'
     )
+
+    list.value.addEventListener('transitionend', () => isDisabled.value = false)
 }
 
 const getNextPrevSibling = ({ element, type = 'next' }: {element: HTMLElement, type?: string}): HTMLElement => {
@@ -63,10 +86,13 @@ onMounted(() => {
     const listCollection = list.value;
     if(!listCollection) return;
 
-    const childs = [listCollection.firstChild, listCollection.lastChild];
+    const collectionChildSize = Array.from(listCollection.childNodes).filter(node => node.nodeName !== '#text')
+    if(collectionChildSize.length === 1) return
+    const childs = [collectionChildSize.at(0), collectionChildSize.at(-1)];
 
-    currentHTMLNode.value = listCollection.firstChild as HTMLElement
+    currentHTMLNode.value = collectionChildSize.at(0) as HTMLElement
 
+    // console.log(childs, listCollection)
     childs.forEach(element => {
         createObserver({
             element: element as HTMLElement,
