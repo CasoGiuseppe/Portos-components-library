0e741cbf96ab6708065dea7826eb89794fab2ba8
@enua <enrique.programador@gmail.com>
2024-04-16 14:11:28 +0200

    (13979): create modal component ready for unit tests

--- src/components/modal/ModalDialog.vue
+++ src/components/modal/ModalDialog.vue
@@ -1,45 +1,48 @@
 <template>
-    <div class="wrapper">
-        <dialog
-            :class="`modal-dialog modal-dialog--is-${size}`"
-            v-on-click-outside="close"
-            v-bind="$attrs"
-        >
+    <dialog
+        ref="dialogRef"
+        :class="`modal-dialog modal-dialog--is-${size}`"
+        @cancel="close"
+        v-bind="$attrs"
+    >
+        <div v-on-click-outside="close">
             <header class="modal-dialog__header">
                 <h2 class="modal-dialog__header--title">
+                    <!-- @slot The title for modal with/without icon-->
                     <slot name="header"></slot>
                 </h2>
 
                 <button class="modal-dialog__header--close" @click="close">
                     <BaseIcon
-                        :id="'CloseIcon '"
+                        :id="'CloseIcon'"
                         :type="Types.NAVIGATION"
                         :name="'IconNavigationCloseM'"
                     ></BaseIcon>
                 </button>
             </header>
-            <section class="modal-dialog__body">
-                <slot></slot>
+            <section>
+                <!-- @slot for default values. The body and footer (buttons)-->
+                <slot name="default"></slot>
             </section>
-            <footer class="modal-dialog__footer">
-                <slot name="actions"></slot>
-            </footer>
-        </dialog>
-    </div>
+        </div>
+    </dialog>
 </template>
 
 <script setup lang="ts">
-import { type PropType, defineProps, ref } from "vue"
+import { type PropType, defineProps, ref, watch, onMounted } from "vue"
 import BaseIcon from "@/components/base/base-icon/BaseIcon.vue"
 import { vOnClickOutside } from "@vueuse/components"
 import { Types } from "../base/base-icon/types"
 import type { UniqueId, SizeType } from "./types"
 
-const emits = defineEmits(["close"])
-const { size } = defineProps({
+const emits = defineEmits(["close", "open"])
+const props = defineProps({
     id: {
         type: String as PropType<UniqueId>,
         default: "modalDialog"
+    },
+    isOpen: {
+        type: Boolean
     },
 
     size: {
@@ -49,82 +52,37 @@ const { size } = defineProps({
     }
 })
 
-const dialogRef = ref<HTMLElement | null>(null)
+watch(
+    () => props.isOpen,
+    (newVal) => {
+        console.log(props.isOpen, newVal)
+        if (newVal) open()
+        else close()
+    }
+)
+
+const dialogRef = ref<HTMLDialogElement | null>(null)
+
+function open() {
+    emits("open")
+    dialogRef.value?.showModal()
+}
 
 function close() {
+    dialogRef.value?.close()
     emits("close")
 }
+
+onMounted(() => {
+    console.log(props.isOpen)
+    if (props.isOpen) open()
+})
+
+// @discuss not sure if should be exposed in the future to manage those from parent components
+defineExpose({
+    open,
+    close
+})
 </script>
 
-<style lang="scss" scoped>
-.wrapper {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    width: 100vw;
-    position: fixed;
-    top: 0;
-    left: 0;
-    background-color: rgb(29, 30, 31, 0.4); //var(--color-neutral-70);
-}
-
-.modal-dialog {
-    display: block;
-    position: relative;
-    box-shadow: var(--shadow-200);
-
-    background-color: var(--color-neutral-10);
-    border: none;
-    border-radius: var(--radius-300);
-    gap: var(--spacing-50);
-    z-index: 99;
-
-    @include text-body-m--regular;
-    color: var(--color-neutral-70);
-
-    &__header {
-        display: flex;
-        justify-content: space-between;
-
-        &--title {
-            @include text-heading-l;
-            font-size: var(--spacing-60);
-        }
-
-        &--close {
-            padding: 0;
-        }
-    }
-
-    &--is {
-        &-wide {
-            width: 80vw;
-        }
-
-        &-narrow {
-            width: 50vw;
-        }
-    }
-
-    &__body {
-        padding: var(--spacing-50) 0;
-    }
-
-    &__footer {
-        padding-top: 16px;
-        display: flex;
-        justify-content: flex-end;
-    }
-
-    .close {
-        position: absolute;
-        right: 10px;
-        top: 10px;
-        border: none;
-        background: none;
-        cursor: pointer;
-        font-size: 24px;
-    }
-}
-</style>
+<style lang="scss" src="./ModalDialog.scss"></style>
