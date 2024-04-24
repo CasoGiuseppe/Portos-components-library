9188f49cb8d3a262d66d6b674511369dddefdfd4
Giuseppe CASO <giuseppecaso@macbook-air-de-servicedesk.home>
2024-04-24 11:40:20 +0200

    refactor: remove unnecessary code

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <template>
     <section>
-        <ToastBox>
-            <template #title> Custom Header</template>
-            This is a very very very very looong text and much more more more large text
-        </ToastBox>
     </section>
 </template>
 
 <script setup lang="ts">
-import ToastBox from './components/tools/toast-box/ToastBox.vue';
 </script>
